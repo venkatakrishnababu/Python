150a32fe964d8aabe57337f4800bc2ec62d22a1b
HP <chunduruvenkatakrishnababu@gmail.com>
2025-02-18 04:14:34 +0530

    main_decor_Convertion

---
+++ decor_main.py
@@ -0,0 +1,22 @@
+def converter(func):
+    """A decorator to convert the case of a name.
+    If the name is all uppercase, it will converts all to lowercase.
+    If the name is all lowercase, it will converts all to uppercase.
+    Otherwise, it returns the same name
+    """
+    def wrapper(name):
+        if name.isupper():
+            return func(name.lower()) #Return lower
+        elif name.islower():
+            return func(name.upper()) #Return upper
+        else:
+            return name #Returns same
+
+    return wrapper
+
+@converter
+def unchanged_name(name):
+    """This function is decorated """
+    return name
+A=unchanged_name('Name')
+print(A)
---
+++ test1.py
@@ -0,0 +1,5 @@
+from decor_main import unchanged_name
+def test1():
+    assert unchanged_name("RAHUL") == "rahul"
+def test2():
+    assert unchanged_name("Rahu") == "Rahu"
